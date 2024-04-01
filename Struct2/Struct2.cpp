75fa00c0abac1ef96d07361b21fc9eba3792cd3e
ZahwaAnritia <anritia15@gmail.com>
2024-04-01 16:17:21 +0700

    Add struct mahasiswa

--- Struct2/Struct2.cpp
+++ Struct2/Struct2.cpp
@@ -6,6 +6,13 @@ struct DetailAlamat {
 	string kota;
 };
 
+struct Mahasiswa {
+	string nim;
+	string nama;
+	DetailAlamat alamat;
+	string umur;
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
