7e09ee6703b6817b7327ad1879e000787ed8b7f5
ZahwaAnritia <anritia15@gmail.com>
2024-04-01 16:34:01 +0700

    input detail alamat mahasiswa

--- Struct3/Struct3.cpp
+++ Struct3/Struct3.cpp
@@ -20,5 +20,11 @@ int main() {
     getline(cin, mhs.nim);
     cout << "Nama Mahasiswa: ";
     getline(cin, mhs.nama);
+
+    cout << "Alamat Mahasiswa : " << endl;
+    cout << "\t Nama Desa:";
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota: ";
+    cin >> mhs.alamat.kota;
 }
 
